5f5ba9e6c9cd90b90d59d289020022971dc73af0
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-09 16:44:42 -0500

    Fix ESLinters

--- modules/dateLuxor.js
+++ modules/dateLuxor.js
@@ -1,5 +1,5 @@
 import { dateElement } from './DOMElements.js';
-import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
+import { DateTime } from './luxon/build/es6/luxon.js';
 
 export default () => {
   const date = DateTime.now().toLocaleString(DateTime.DATE_FULL);
