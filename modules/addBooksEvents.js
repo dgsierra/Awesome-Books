0395541b31fd272bcdcb4f5e1a0150088ceb74a3
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-08-09 11:15:36 -0500

    Checkpoint, pending button and getitem sections.

--- modules/addBooksEvents.js
+++ modules/addBooksEvents.js
@@ -1,17 +1,17 @@
-import { button } from './DOMElements.js';
-import dataform from './localStorage.js';
+import { button, bookTitle,  } from './modules/DOMElements.js';
+import { dataform } from './modules/localStorage.js';
 
-export default () => {button.addEventListener('click', (e) => {
-    e.preventDefault();
-    const book = bookTitle.value;
-    const author = authorName.value;
-    books.addBook(book, author);
-    books.removeBook();
-    // RESET-VALUES
-    authorName.value = '';
-    bookTitle.value = '';
-    // LOCAL-STORAGE
-    dataform();
-    // COUNTER-TRACKER
-    books.counter += 1;
-  })};
+// export default button.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     const book = bookTitle.value;
+//     const author = authorName.value;
+//     books.addBook(book, author);
+//     books.removeBook();
+//     // RESET-VALUES
+//     authorName.value = '';
+//     bookTitle.value = '';
+//     // LOCAL-STORAGE
+//     dataform();
+//     // COUNTER-TRACKER
+//     books.counter += 1;
+//   });
